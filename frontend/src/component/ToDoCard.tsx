6f1b33796e21b0c759c59cca2c134df78518d42a
Tobias Weber <toby-weber@gmx.de>
2024-11-08 15:43:29 +0100

    add update todo option

--- frontend/src/component/ToDoCard.tsx
+++ frontend/src/component/ToDoCard.tsx
@@ -1,6 +1,7 @@
 import "./ToDoCard.css"
 import {ToDo} from "../ToDo.ts";
 import axios from "axios";
+import {ChangeEvent, useState} from "react";
 
 
 type Props = {
@@ -10,11 +11,21 @@ type Props = {
 
 export default function ToDoCard(props: Props) {
 
+    const [description,setDescription] = useState(props.todo.description)
+
     function deleteToDo(){
         axios.delete("api/todo/"+props.todo.id)
             .then(props.updateToDoList)
     }
-    function updateToDo(){
+    function updateToDo(event: ChangeEvent<HTMLInputElement>){
+
+        setDescription(event.target.value)
+        console.log(event.target.value)
+        axios.put("api/todo/"+props.todo.id,{
+            ...props.todo,
+            description:event.target.value
+        })
+            .then(props.updateToDoList)
 
     }
     function moveToDoLeft(){
@@ -26,9 +37,8 @@ export default function ToDoCard(props: Props) {
 
     return (
         <div className="todo-card">
-            <p>{props.todo.description}</p>
+            <input value={description} onInput={updateToDo}/>
             <button onClick={moveToDoLeft}>move left</button>
-            <button onClick={updateToDo}>update</button>
             <button onClick={deleteToDo}>delete</button>
             <button onClick={moveToDoRight}>move right</button>
         </div>
